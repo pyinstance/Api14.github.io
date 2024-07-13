1dba3ab750fc3072073b617bb96c03ce5f42252b
Api <86266617+Api14@users.noreply.github.com>
2024-07-13 03:46:23 +0100

    Add files via upload

---
+++ lust/pastes/harryfrench.js
@@ -0,0 +1,283 @@
+var fileContent = `
+
+┬┌─┐┬┌─┐
+│└─┐│└─┐
+┴└─┘┴└─┘
+Doxxed By lust
+
+~ REASON FOR DOX ~
+  ~ Racism 
+  ~ Card Theft / Fraud
+  ~ Cyber Bullying 
+  ~ Child Pornography / On a kid thats 10 Years old
+==================================================================================================
+
+His Passwords [ https://www.mediafire.com/file/j60eqjln9z071vx/wppassw_%25282%2529.txt/file ]
+Personal Logins [ https://www.mediafire.com/file/v8l6m69mhwmd256/logins.txt/file ]
+Cookies Logs [ https://www.mediafire.com/file/yfm62xjdwu2entw/wpcook.txt/file ]
+
+====================== Proof of Fraud / Theft =====================================================
+Picture 1 ~ https://cdn.discordapp.com/attachments/1042520838123032631/1042857233928695869/image.png
+Picture 2 ~ https://cdn.discordapp.com/attachments/1042520838123032631/1042857248902365284/image.png
+====================== Proof of Racism ===========================
+Picture 1 ~ https://cdn.discordapp.com/attachments/1010597192542728362/1042858302402482227/image.png
+====================== Main Target ===================
+{ Name } ~ Harry French
+{ Discord User } ~ ꜱᴘɪᴛꜰɪʀᴇ#8484
+{ Discord Token } ~ Njg5ODQ1NzU4MzY2NzExOT***********
+{ Discord Token } ~ MTAzMDQ3NzIzMDMwMTc4Mj***********
+{ Age } ~ 19 
+{ IP } ~  185.193.170.240 { Sat on a 4G connection }
+{ Location } ~ Boadring School ~ > Blandford Forum DT11 0PX < Approx 2 hours from His House
+{ Telephone } ~ 01258452411 <- School
+{ Address } ~  Burpham, Arundel BN18 9RJ
+
++ He normally Gose home on the weekends +
+===================== Passwords ======================
+[ School ] https://bryanston.co.uk/bryanston_pupil_provisioning.php 
+~ Username: french_h 
+~ Password: 1Biggles
+~ Video Of Making a report { https://cdn.discordapp.com/attachments/985557747099652126/1042854339179458691/XXXXX.mp4 }
+======================================================
+                  ~ Other INFO ~
+
+Url: http://192.168.1.241:1000/ | Username: french_h | Password: 1Biggles
+Url: https://login.live.com/ppsecure/post.srf | Username: harryfrench1482@gmail.com | Password: 1Biggles.
+Url: https://manage.gghosting.cf/login | Username: hfrench1482@gmail.com | Password: 1Dragons.GGhosting
+Url: https://servers.gghosting.cf/auth/login | Username: hfrench1482@gmail.com | Password: 1Dragons.GGhosting
+Url: https://gameservershub.com/forums/register/register | Username: Harry1482 | Password: 1Dragons.plugins
+Url: https://gameservershub.com/forums/account/email | Username: harryfrench1482@gmail.com | Password: 1Dragons.plugins
+Url: https://www.roblox.com/login/reset-password | Username:  | Password: 1Dragons.Roblox
+Url: https://www.digitalcombatsimulator.com/en/auth/ | Username: Harry1482 | Password: 1Dragons.DCS
+Url: https://www.freecycle.org/login | Username: Harry1482 | Password: 1Dragons.freecycle
+Url: https://eu.account.battle.net/creation/flow/patch/step/set-password | Username:  | Password: 1Dragons.battlenet
+Url: https://vacban.wtf/login/login | Username: Harry1482 | Password: 1Dragons.vacban
+Url: https://cheater.net/register-post | Username: Harry1482 | Password: 1Dragons.cheater
+Url: https://login.live.com/ppsecure/post.srf | Username: harryfrench2007@hotmail.com | Password: 1Biggles.
+Url: https://www.epicgames.com/id/login/reset-password | Username:  | Password: 1Dragons.epicGames
+Url: https://github.com/signup | Username: Harry1482 | Password: 1Dragons.GitHub
+Url: https://login.microsoftonline.com/07b83d75-44e9-45c1-b70e-47f6eb6a29f4/login | Username: french_h@bryanston.co.uk | Password: 1Biggles
+Url: https://mega.nz/confirmQ29uZmlybUNvZGVWMhZSYID9AwADdAwWe9gBOGhmcmVuY2gxNDgyQGdtYWlsLmNvbQlIYXJyeSBGcmVuY2iwEFCj-oH70A | Username: hfrench1482@gmail.com | Password: 1Dragons.Mega
+Url: https://mega.nz/register | Username: hfrench1482@gmail.com | Password: 1Dragons.Mega
+Url: https://www.coinbase.com/signup | Username: hfrench1482@gmail.com | Password: 1Dragons.Coinbase
+Url: https://login.coinbase.com/signin | Username: hfrench1482@gmail.com | Password: 1Dragons.Coinbase
+Url: https://battlelog.co/register/ | Username: Harry1482 | Password: 1Dragons.BattleLog.co
+Url: https://paxful.com/register | Username: hfrench1482@gmail.com | Password: 1Dragons.Paxful
+Url: https://discord.com/channels/@me | Username: Slutty Girl | Password: Oreo2021?
+Url: https://app.hackthebox.com/login | Username: harryfrench1482@gmail.com | Password: 1Dragons.HackTheBox
+Url: https://www.hackthebox.com/password/reset | Username: harryfrench1482@gmail.com | Password: 1Dragons.HackTheBox
+
+===================== Contact Info ===================
+[ Email 1 ] harryfrench1482@gmail.com
+[ Email 2 ] hfrench1482@gmail.com
+[ Phone ] +447306077296
+===================== Billing info ===================
+               ~ His Payments methods ~ 
+
+           Valid                  True
+           PayPal Name            Harry French
+           PayPal Email           harryfrench1482@gmail.com
+           Address 1              Absuma
+           Address 2              Burpham
+           City                   Arundel
+           Postal Code            BN18 9RJ
+           Country                GB
+           
+
+            ~ Uncles Payment Methods ~
+
+        CC Holder Name         Edward French
+        CC Brand               Mastercard
+        CC Number              52 11 41 13 39 42 83 95
+        CC Exp. Date           08/27
+        Address 1              Absuma
+        City                   Arundel
+        Postal Code            BN189RJ
+        State                  West Sussex
+        Country                GB
+
+======================= His Uncle ==========================
+{ Name } ~ Edward French
+{ Age } ~ 40-50s
+{ Adress } ~ Arundel, Arundel, bn18 9rj
+{ Email } ~ ed_french@hotmail.com
+{ Mobile } ~ 07796265757
+{ Telephone } ~ 01903884459
+=============================================================
+
+
+                       ~ Enjoy ~
+
+
+======================= School INFO ========================
+~ Bryanston School
+~ Blandford Forum
+~ Dorset
+~ DT11 0PX
+================ General Enquiries ======================
+~ General Enquiries
+~ Tel: + 44 (0)1258 452 411
+~ Email: post@bryanston.co.uk
+================ Admissions Office ======================
+~ Admissions Office
+~ Tel: + 44 (0)1258 484 633
+~ Admissions enquiry form
+================ Head ====================
+~ Head, Richard Jones
+~ Tel: +44 (0)1258 484 632
+~ Email: headmaster@bryanston.co.uk
+
+======================= IP INFO  ===========================
+[ IP INFO ] 
+IP address	185.193.170.240
+Host name	185.193.170.240.customer.ranston.net
+IP range	185.193.170.128-185.193.170.255 CIDR
+ISP	        Wessex Internet Limited
+Organization	Wessex Internet Limited
+Country	 United Kingdom of Great Britain and Northern Ireland (GB)
+Region	England 
+============================== MAC Address's  =====================================      
+[ MAC ADDRESS ]
+02-92-A4-E7-0F-C8   \Device\Tcpip_{3C07D5F4-4995-4408-B109-90E1907E93CC
+D8-BB-C1-23-0A-80   Disconnected
+F0-9E-4A-5E-44-98   Media disconnected
+00-FF-7A-62-2C-BD   Media disconnected
+=============================== MAC Address Info ===================================
+Result for: F0-9E-4A-5E-44-98
+Address Prefix	F0:9E:4A
+Vendor / Company	Intel Corporate
+Start Address	F09E4A000000
+End Address	F09E4AFFFFFF
+Company Address	Lot 8, Jalan Hi-Tech 2/3 Kulim Kedah 09000 My
+                   ~ In depth Lookup ~
+Vendor details
+
+OUI ~ F0:9E:4A | Is private = False | Company name Intel Corp
+
+Company address Lot 8, Jalan Hi-Tech 2/3 Kulim Kedah 09000 MY
+
+Country code = MY | Block details = Is registered = True
+
+Border left = F0:9E:4A:00:00:00 | Border right = F0:9E:4A:FF:FF:FF
+
+Block size = 16,777,216 | Assignment block size MA-L | Date created = 10 December 2020
+
+Date updated = 10 December 2020 | MAC address details = Is valid / True | Virtual Machine = Not detected 
+
+Transmission type = Unicast | Administration type = UAA Applications = Not detected
+
+Wireshark notes = No details
+
+============================== Device Hosts / Connections ============================
+  Connection-specific DNS Suffix  . : school.bryanston
+  Link-local IPv6 Address . . . . . : fe80::dfbd:1e73:6aca:3b03%7
+  IPv4 Address. . . . . . . . . . . : 172.16.37.53
+  Subnet Mask . . . . . . . . . . . : 255.255.248.0
+  Default Gateway . . . . . . . . . : 172.16.32.1
+                       
+                         ~ IPv4 Lookup ~
+
+NetRange:       172.16.0.0 - 172.31.255.255
+CIDR:           172.16.0.0/12
+NetName:        PRIVATE-ADDRESS-BBLK-RFC1918-IANA-RESERVED
+NetHandle:      NET-172-16-0-0-1
+Parent:         NET172 (NET-172-0-0-0-0)
+NetType:        IANA Special Use
+OriginAS:       
+Organization:   Internet Assigned Numbers Authority (IANA)
+RegDate:        1994-03-15
+Updated:        2013-08-30
+Comment:        These addresses are in use by many millions of independently operated networks, which might be as small as a single computer connected to a home gateway, and are automatically configured in hundreds of millions of devices.  They are only intended for use within a private context  and traffic that needs to cross the Internet will need to use a different, unique address.
+Comment:        
+Comment:        These addresses can be used by anyone without any need to coordinate with IANA or an Internet registry.  The traffic from these addresses does not come from ICANN or IANA.  We are not the source of activity you may see on logs or in e-mail records.  Please refer to http://www.iana.org/abuse/answers
+Comment:        
+Comment:        These addresses were assigned by the IETF, the organization that develops Internet protocols, in the Best Current Practice document, RFC 1918 which can be found at:
+Comment:        http://datatracker.ietf.org/doc/rfc1918
+Ref:            https://rdap.arin.net/registry/ip/172.16.0.0
+
+
+
+OrgName:        Internet Assigned Numbers Authority
+OrgId:          IANA
+Address:        12025 Waterfront Drive
+Address:        Suite 300
+City:           Los Angeles
+StateProv:      CA
+PostalCode:     90292
+Country:        US
+RegDate:        
+Updated:        2012-08-31
+Ref:            https://rdap.arin.net/registry/entity/IANA
+
+
+OrgAbuseHandle: IANA-IP-ARIN
+OrgAbuseName:   ICANN
+OrgAbusePhone:  +1-310-301-5820 
+OrgAbuseEmail:  abuse@iana.org
+OrgAbuseRef:    https://rdap.arin.net/registry/entity/IANA-IP-ARIN
+
+OrgTechHandle: IANA-IP-ARIN
+OrgTechName:   ICANN
+OrgTechPhone:  +1-310-301-5820 
+OrgTechEmail:  abuse@iana.org
+OrgTechRef:    https://rdap.arin.net/registry/entity/IANA-IP-ARIN
+
+                       ~ Conversions ~ 
+
+IP v4 :
+decimal	:  172.16.37.53
+binary	:  10101100000100000010010100110101
+octal	:  0254.020.045.065
+hexadecimal	:  0xAC.0x10.0x25.0x35
+long	:  2886739253
+
+IP v6 :
+6 to 4 address	:  2002:AC10:2535:0:0:0:0:0
+:  2002:AC10:2535::
+IPv4-mapped address	:  0:0:0:0:0:FFFF:172.16.37.53
+:  ::FFFF:172.16.37.53
+:  ::FFFF:AC10:2535
+IPv4-compatibility address	:  0:0:0:0:0:0:172.16.37.53
+:  ::172.16.37.53
+:  ::AC10:2535
+===========================================================================
+                     ~ Associated IPs ~
+IP address	Type	Host name	DNS state
+104.47.30.74	 MX 	ranston-net.mail.protection.outlook.com	
+198.41.0.4	 NS 	a.root-servers.net	
+199.9.14.201	 NS 	b.root-servers.net	
+192.33.4.12	 NS 	c.root-servers.net	
+199.7.91.13	 NS 	d.root-servers.net	
+192.203.230.10	 NS 	e.root-servers.net	
+192.5.5.241	 NS 	f.root-servers.net	
+192.112.36.4	 NS 	g.root-servers.net	
+198.97.190.53	 NS 	h.root-servers.net	
+192.36.148.17	 NS 	i.root-servers.net	
+192.58.128.30	 NS 	j.root-servers.net	
+193.0.14.129	 NS 	k.root-servers.net	
+199.7.83.42	 NS 	l.root-servers.net	
+202.12.27.33	 NS 	m.root-servers.net	
+199.180.182.53	 NS 	a.in-addr-servers.arpa	
+199.253.183.183	 NS 	b.in-addr-servers.arpa	
+196.216.169.10	 NS 	c.in-addr-servers.arpa	
+200.10.60.53	 NS 	d.in-addr-servers.arpa	
+203.119.86.101	 NS 	e.in-addr-servers.arpa	
+193.0.9.1	 NS 	f.in-addr-servers.arpa
+
+===================================================
+
+         ~ ASN Number Lookup ~
+206347	Wessex Internet Limited	37.48.225.0/24
+206347	Wessex Internet Limited	37.48.226.0/23
+206347	Wessex Internet Limited	37.48.228.0/24
+206347	Wessex Internet Limited	37.48.231.0/24
+206347	Wessex Internet Limited	138.124.160.0/21
+206347	Wessex Internet Limited	138.124.168.0/24
+206347	Wessex Internet Limited	138.124.175.0/24
+206347	Wessex Internet Limited	185.67.232.0/22
+206347	Wessex Internet Limited	185.102.132.0/22
+206347	Wessex Internet Limited	185.193.168.0/22
+206347	Wessex Internet Limited	194.49.12.0/22
+
+============= End Of DOX ==================
+`;
